9442d555e2d8b344282bea03436b14eba3d17c72
João Victor <jvaarantes@gmail.com>
2024-01-31 12:41:50 -0300

    Adicionar arquivos de projeto.

---
+++ ExerciciosPOO8/Empregado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciciosPOO8
+{
+    internal class Empregado
+    {
+        public int Id { get; set; }
+        public String Nome { get; set; }
+        public double Salario { get; private set; }
+
+        public Empregado(int Id, String Nome, Double Salario)
+        {
+            this.Id = Id;
+            this.Nome = Nome;
+            this.Salario = Salario;
+        }
+        public void AumentoSalario(double porcentagem)
+        {
+            Salario += Salario * (porcentagem / 100);
+        }
+        public override string ToString()
+        {
+            return $"Id: {Id}; Nome: {Nome}; Salario: {Salario.ToString("F2")} \n";
+        }
+    }
+}
---
+++ ExerciciosPOO8/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace ExerciciosPOO8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Empregado> list = new List<Empregado>();
+
+            Console.WriteLine("Quantos funcionários serão registrados?");
+            int aux = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < aux; i++)
+            {
+                Console.WriteLine($"Empregado #{i+1}");
+                Console.Write("ID:");
+                int Id = Convert.ToInt32(Console.ReadLine());
+
+                Console.Write("Nome:");
+                String Nome = Console.ReadLine();
+
+                Console.Write("Salario:");
+                double Salario = Convert.ToDouble(Console.ReadLine());
+
+                list.Add(new Empregado(Id, Nome, Salario));
+            }
+
+            Console.WriteLine("Digite a ID do funcionário que receberá um aumento:");
+            int VerificaID = Convert.ToInt16(Console.ReadLine());
+
+            Empregado emp = list.Find(x => x.Id == VerificaID);
+            
+            if (emp != null)
+            {
+                Console.Write("Digite a porcentagem do ajuste no salário: ");
+                double Porcentagem = Convert.ToDouble(Console.ReadLine());
+                
+                emp.AumentoSalario(Porcentagem);
+            }
+            else
+            {
+                Console.WriteLine("Digite um valor válido.");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("Lista atualizada de empregados: ");
+            Console.WriteLine();
+
+            foreach (Empregado obj in list)
+            {                
+                Console.WriteLine(obj.ToString());
+            }
+
+            Console.ReadKey();
+            Console.Beep();
+        }
+    }
+}
